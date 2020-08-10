19392bc8e2420bc3900413b9404e548beb4481ad
Dariusz <darek200180@gmail.com>
2020-08-10 23:32:57 +0100

    add some bootstrap classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,15 +7,14 @@ class Counter extends Component {
 
     formatCount() {
         const { count } = this.state;
-
-    return count === 0 ? "Zero" : count;
+        return count === 0 ? "Zero" : count;
     }
 
     render() { 
         return ( 
             <React.Fragment>
-                <span>count: {this.formatCount()}</span>
-                <button>Increment</button>
+                <span className="badge badge-primary m-2">count: {this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
          );
     }
