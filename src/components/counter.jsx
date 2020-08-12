4de7a45b802b297c369085dbcf20d179af0faad8
Dariusz <darek200180@gmail.com>
2020-08-12 23:59:35 +0100

    add counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,10 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        count: this.props.value,
     }
 
     handleIncrement = (product) => {
-        console.log(product);
 
         this.setState({
             count: this.state.count + 1
@@ -15,25 +13,15 @@ class Counter extends Component {
     }
 
     render() { 
+        console.log(this.props)
+
         return ( 
-            <React.Fragment>
+            <div>
                 <span className={this.getBadgeClasses()}>count: {this.formatCount()}</span>
                 <button onClick={() => this.handleIncrement({ id: 123 })} className="btn btn-secondary btn-sm">Increment</button>
                 
-                { this.renderTags() }
-            </React.Fragment>
+            </div>
          );
-    }
-
-    renderTags() {
-        if(this.state.tags.length === 0)
-            return <p>There are no tags</p>
-        
-        return(
-            <ul>
-                { this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }
-            </ul>
-        )
     }
 
     getBadgeClasses() {
