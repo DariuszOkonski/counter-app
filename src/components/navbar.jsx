225fd7661fd932b51a6cef5692fa9c7a85ecfa40
Dariusz <darek200180@gmail.com>
2020-08-14 22:57:30 +0100

    move state from counters to app component and send it as a props

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,10 @@ class Navbar extends Component {
     render() { 
         return ( 
             <nav className="navbar navbar-light bg-light">
-                <a href="#" className="navbar-brand">Navbar</a>
+                <a href="#" className="navbar-brand">
+                    Navbar{" "}
+                    <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+                </a>
             </nav>
          );
     }
